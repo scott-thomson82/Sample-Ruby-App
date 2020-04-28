d12e67b31918a70109aab92fe8d23cc1ef33bb77
Scott Thomson <scottthomson_57@hotmail.com>
2020-04-28 18:07:28 +1200

    Finish Static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello 
-  render text: "hello, world"
+  render plain: "hello, world"
   end
 end
 
