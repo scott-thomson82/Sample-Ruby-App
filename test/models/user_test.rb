8052384e0ead0de3c44d0d86991a569406ee5f20
Scott Thomson <scottthomson_57@hotmail.com>
2020-06-01 19:49:34 +1200

    completing email validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -38,12 +38,9 @@ require 'test_helper'
       end
     end
 
-    test "email addresses should be unique" do
-      duplicate_user = @user.dup
-      @user.save
-      assert_not duplicate_user.valid?
-    end
-  end
+    
+ 
+  
   
 
 
