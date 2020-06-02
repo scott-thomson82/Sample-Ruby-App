803498c15a9990453ba1add95ea2189941f421c1
Scott Thomson <scottthomson_57@hotmail.com>
2020-06-02 20:33:38 +1200

    making a basic user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
  class UserTest < ActiveSupport::TestCase
 
  def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+    password: "foobar", password_confirmation: "foobar")
  end
 
  test "name should be valid" do
@@ -37,6 +38,17 @@ require 'test_helper'
         assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
       end
     end
+
+    test "password should be present (nonblank)" do
+      @user.password = @user.password_confirmation = " " * 6
+      assert_not @user.valid?
+    end
+    
+     test "password should have a minimum length" do
+      @user.password = @user.password_confirmation = "a" * 5
+      assert_not @user.valid?
+    end
+  end
 
     
  
